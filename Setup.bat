9c358afc3133706f07ef88fb55ba74b5437896c0
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2021-01-21 08:21:07 -0500

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,12 @@
+echo Installing numpy, pyttsx3, warings, matplotlib, PIL, cv2, tensorflow, keras
+pip
+pip install --upgrade pip
+pip install numpy
+pip install pyttsx3
+pip install matplotlib
+echo Halfway done
+pip install opencv-python
+pip install keras
+pip install python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
+echo Done
+color 2
